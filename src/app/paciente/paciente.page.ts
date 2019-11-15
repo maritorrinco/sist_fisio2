d81ae194bd6905ed58b9b16017d018232c0318b2
Maricris <maricris.orue@gmail.com>
2019-11-14 23:26:51 -0300

    Fix lista pacientes

--- src/app/paciente/paciente.page.ts
+++ src/app/paciente/paciente.page.ts
@@ -7,14 +7,16 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./paciente.page.scss'],
 })
 export class PacientePage implements OnInit {
+  public response: any;
   public items: Array<{ nombre: string }> = [];
 
   constructor(private http: HttpClient) { 
     this.http.get('http://gy7228.myfoscam.org:8080/stock-pwfe/persona').subscribe((response) => {
       console.log(response);
-      for (let i = 0; i < response.totalDatos; i++) {
+      this.response = response;
+      for (let i = 0; i < this.response.totalDatos; i++) {
         this.items.push({
-          nombre: response.lista[i].nombre,
+          nombre: this.response.lista[i].nombre,
           });
       }
     });
